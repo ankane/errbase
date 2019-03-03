2cb42c9feb3ac14bfa79ef20b0cefe6b2198bdd2
Andrew Kane <andrew@chartkick.com>
2019-03-03 13:21:55 -0800

    Removed support for Exceptional and Opbeat, take 2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,12 +7,10 @@ require "minitest/pride"
 require "airbrake"
 require "appsignal"
 require "bugsnag"
-# require "exceptional" # raises exception when not configured
 require "exception_notification"
 # require "google/cloud/error_reporting" # raises exception when not configured
 require "honeybadger"
 require "newrelic_rpm"
-require "opbeat"
 # require "raygun4ruby" # raises exception when not configured
 require "rollbar"
 require "sentry-raven"
