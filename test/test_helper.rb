5d0963c7ad18d66ae58075c9eb8f95463dacf745
Andrew Kane <andrew@chartkick.com>
2019-03-03 13:08:52 -0800

    Updated order [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,15 +2,15 @@ require "bundler/setup"
 Bundler.require(:default)
 require "minitest/autorun"
 require "minitest/pride"
-require "rollbar"
 require "airbrake"
-require "honeybadger"
-# require "exceptional" # raises exception when not configured
-# require "raygun4ruby" # raises exception when not configured
-require "sentry-raven"
-require "bugsnag"
 require "appsignal"
-require "opbeat"
+require "bugsnag"
+# require "exceptional" # raises exception when not configured
 require "exception_notification"
-require "newrelic_rpm"
 # require "google/cloud/error_reporting" # raises exception when not configured
+require "honeybadger"
+require "newrelic_rpm"
+require "opbeat"
+# require "raygun4ruby" # raises exception when not configured
+require "rollbar"
+require "sentry-raven"
